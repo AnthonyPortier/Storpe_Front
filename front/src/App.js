a7fa1bcbf78d2d07c2d36f7a9cc06a9dd9c8f77d
Alexis ARCHER <alexis.archer44@gmail.com>
2019-12-18 23:58:00 +0100

    algo ok en attente de database + yacine est magique

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react';
 import {ContextHook} from './components/useContext'
 import Router from './components/Router'
+import MatchPage from './components/1_matchPage/MatchPage'
 
 function App() {
   const [ranking, setRanking] = useState()
@@ -12,6 +13,7 @@ function App() {
     <ContextHook.Provider value={{ranking, setRanking}}>
         <div className="app">
         <Router/>
+        <MatchPage/>
         </div>
     </ContextHook.Provider>
   );
