4c692aacb4c4a8194d6e8fb195c4e899fe98c4ef
Alexis ARCHER <alexis.archer44@gmail.com>
2019-12-18 19:00:45 +0100

    useContext contenant setRanking

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import {ContextHook} from './components/useContext'
 
 function App() {
-  return (
-    <div className="app">
-      <Router/>
-    </div>
+  const [ranking, setranking] = useState(initialState)
+    return (
+
+
+    <ContextHook.Provider value={{ranking, setRanking}}>
+        <div className="app">
+        <Router/>
+        </div>
+    </ContextHook.Provider>
   );
 }
 
