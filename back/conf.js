66373b1d39ba2f20f262a7a03422e9b9210bb509
Alexis ARCHER <alexis.archer44@gmail.com>
2019-12-18 17:27:07 +0100

    suppress de back

--- back/conf.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const connexion = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "toBeDetermined",
-database: "tobeCompleted"
-})
-
-module.exports=connexion
