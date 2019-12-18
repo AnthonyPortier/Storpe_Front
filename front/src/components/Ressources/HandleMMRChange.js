f81af3c54576913a3ef909529276ba9f43edfca6
Alexis ARCHER <alexis.archer44@gmail.com>
2019-12-18 18:45:59 +0100

    algo d'attibution des points. A finaliser en fonction des cotes données

---
+++ front/src/components/Ressources/HandleMMRChange.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+
+    const handleMMRChange = (result) =>{
+        const cote = 1.10
+        let attributedPoints = 0
+
+        if (result === 'victory'){
+            cote>=1.10?
+                attributedPoints = cote*10:
+                attributedPoints =cote*100-100
+
+        }
+
+        else if (result === 'draw'){
+
+
+
+        }
+
+        else if (result === 'defeat'){
+            switch (cote) {
+                case cote>2.60:
+                    attributedPoints=-5
+                break;
+            
+                case cote>2.30:
+                    attributedPoints=-6
+                break;
+
+                case cote>2.10:
+                    attributedPoints=-7
+                break;
+
+                case cote>1.86:
+                    attributedPoints=-7
+                break;
+
+                case cote>1.66:
+                    attributedPoints=-8
+                break;
+
+                case cote>1.50:
+                    attributedPoints=-10
+                break;
+
+                case cote>1.3:
+                    attributedPoints=-11
+                break;
+
+                case cote>1.1:
+                    attributedPoints=-15
+                break;
+
+
+                default:
+                    break;
+            }
+
+        }
+
+        else{
+            "ERREUR"
+        }
+
+        return(attributedPoints)
+    }
+
+    
+    
+
+export default handleMMRChange;
