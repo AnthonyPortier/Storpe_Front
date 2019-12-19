513542f58aa2a846d0c661f3e748b45d42a1f797
Alexis ARCHER <alexis.archer44@gmail.com>
2019-12-19 14:19:03 +0100

    ajout bouton valider, en attente uniformisation dylan

--- front/src/components/1_matchPage/MatchPage.js
+++ front/src/components/1_matchPage/MatchPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, Button} from 'reactstrap'
 import HandleMMRChange from '../Ressources/HandleMMRChange'
 import ReverseCoting from '../Ressources/reverseCoting'
@@ -6,8 +6,11 @@ import '../1_matchPage/MatchPage.css'
 
 
 const MatchPage = () => {
-
-
+    const [Counter, setCounter] = useState(0)
+    const decimal1 = (number)=>{
+        return(
+        Math.round(number*100)/100
+        )}
 
     const dataBaseMatch = {
             
@@ -22,23 +25,23 @@ const MatchPage = () => {
             cote : 1.4},
     }
 
-    const equipeImages = [
-        ["Paris-Saint-Germain", "/home/alex/Documents/Hackathon2/storpe/front/src/img/1024px-Paris_Saint-Germain_Logo.svg.png"]
-    ]
     
-    return (  
+    return (
+    <>  
         <div className='MatchPage-full'>
             <h1 className='h1-title'>Les matchs du jours</h1>
                 <h2 className='h2-title'>FOOTBALL</h2>
+        </div>
         <div className="MatchCards">
-    {/*         <img alt="" style={{width:"140px"}} src={require(equipeImages[0][1])}/>*/}
+
+
             {Object.values(dataBaseMatch).map(x=>
                 <Card>
                     <div className="cardBody">
                         <div className="Team">
                             <div className="teamHead">
                                 <h3>{x.EquipeDomicile}</h3>
-                                {x.cote}
+                                {decimal1(x.cote)}
                             </div>
                             <div className="MMR">
                                 <Button>
@@ -57,27 +60,35 @@ const MatchPage = () => {
                         
                         <div className="Team">
                             <div className="teamHead">
-                                <h3>{x.EquipeDomicile}</h3>
-                                {x.cote}
+                                <h3>{x.EquipeVisiteur}</h3>
+                                {decimal1(ReverseCoting(x.cote))}
                             </div>
                             <div className="MMR">
                                 <Button>
-                                    Bet for {x.EquipeDomicile}
+                                    Bet for {x.EquipeVisiteur}
                                 </Button>
                                 <div className="points">
-                                    <p>+ {HandleMMRChange('victory', x.cote)}</p>
-                                    <p>- {HandleMMRChange('defeat', x.cote)}</p>
-                                </div>                 
+
+                                    <p>+ {HandleMMRChange('victory', ReverseCoting(x.cote))}</p>
+                                    <p>- {HandleMMRChange('defeat', ReverseCoting(x.cote))}</p>
                                 </div>
                         </div>
                     </div>
+                </div>
                 </Card>
+            )}
+                <div className="sideBarRight">
+                    <button type='submit'>
+                        Valider
+                    </button>
                 
-                )}
+                    <p> {Counter}/10 </p>
+
+                </div>
+                
         </div>   
-        </div>
 
-
+    </>
     );
 }
  
