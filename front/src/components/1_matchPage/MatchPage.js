f9d7f4d58bee667322214165f9e9d1ba0989e09c
Alexis ARCHER <alexis.archer44@gmail.com>
2019-12-20 10:43:37 +0100

    ok slide

--- front/src/components/1_matchPage/MatchPage.js
+++ front/src/components/1_matchPage/MatchPage.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Slider from 'react-slick'
+import Slide1 from './Slide1'
+import Slide2 from './Slide2'
+import './MatchPage.scss'
+
 
 const MatchPage = () => {
+
+    /* settings du slider */
+    const settings = {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+
     return (  
-        ""
+        <div className='MatchPage-full'>
+            <h1 className='h1-title'>Les matchs du jours</h1>
+            <Slider style={{width: '90%', margin : 'auto'}} >
+                <Slide1/>
+                <Slide2/>
+            </Slider> 
+        </div>
+
     );
 }
  
